f40d09fce75c51e123ba5f11b360f4ffe2a6b09a
VishShindhe <vishwa.shindhe@gmail.com>
2024-03-02 18:02:50 +0530

    Added LocatorsTests

--- tests/locators.spec.ts
+++ tests/locators.spec.ts
@@ -1,3 +1,15 @@
-test('', async ({ page }) :Promise<void> => {
+import { expect, test} from '@playwright/test';
 
-});
+test('locators demo', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  
+    // Find the username field and enter value
+   // await page.getByRole("textbox").first().fill("Admin");
+   
+    await page.getByPlaceholder(/username/i).fill("Admin");
+    await page.getByRole("button",{name: /login/i});
+    await expect(page.getByAltText(/company-branding/i)).toBeVisible();
+    await expect(page).toHaveTitle(/OrangeHRM/i);
+    await page.getByText(/Forgot your password/i).click();
+    await page.close();
+  });
