66a5a50a7c47a8dc8a8135cc61ed3e2ebcf20ed4
VishShindhe <vishwa.shindhe@gmail.com>
2024-03-06 23:19:15 +0530

    added waitForRequest and waitForesponse

--- tests/waits.spec.ts
+++ tests/waits.spec.ts
@@ -3,8 +3,17 @@ import { test, expect } from '@playwright/test';
 test('wait demo', async ({ page }): Promise <void> => {
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-  await page.waitForURL(/login/);
+  /*await page.waitForURL(/login/);
   await page.waitForTimeout(3000);
-  await parseArgs.getByPlaceholder("username12").fill(dsds);
-  await expect(page).toHaveTitle(/Playwright/);
+  await page.getByPlaceholder("username12").fill(dsds);
+  await expect(page).toHaveTitle(/Playwright/);*/
+
+  //const request = await page.waitForRequest(/ohrm_logo.png/);
+  //console.log(request.url);
+  const response = await page.waitForResponse(/ohrm_logo.png/);
+  console.log(response.request().url());
+  await expect(page.getByAltText("orangehrm-logo").last()).toBeVisible({timeout: 5});
+  
+
+
 });
